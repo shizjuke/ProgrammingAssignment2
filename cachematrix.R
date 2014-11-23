146c7064e9057ae88432e8aa74596419fd4b66a9
AW <shizjuke@wp.pl>
2014-11-23 18:03:29 +0100

    comments added

--- cachematrix.R
+++ cachematrix.R
@@ -1,33 +1,62 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Matrix inversion is usually a costly computation and their may be some benefit 
+## to caching the inverse of a matrix rather than compute it repeatedly. 
+## Below are two functions that are used to create a special object that stores 
+## a matrix and cache's its inverse.
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
   inv <- NULL
+  
+  # set the value of the matrix
   set <- function(y) {
     x <<- y
     inv <<- NULL
   }
+  
+  # get the value of the matrix
   get <- function() x
+  
+  # set the value of the inverse of a matrix
   setinverse <- function(inverse) inv <<- inverse
+  
+  # get the value of the inverse of a matrix
   getinverse <- function() inv
+  
+  # return special matrix
   list(set = set, get = get,
        setinverse = setinverse,
        getinverse = getinverse)
 }
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned 
+## by makeCacheMatrix above. If the inverse has already been calculated 
+## (and the matrix has not changed), then the cachesolve should retrieve 
+## the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
   inv <- x$getinverse()
+  
+  ## check if the inverse of a matrix has already been calculated
   if(!is.null(inv)) {
-    message("getting cached data")
+    
+    message("getting cached inverse")
+    
+    ## if the inverse of a matrix was calculated then return cached value
+    ## and skip the computation
     return(inv)
   }
+  
+  ## if the inverse of a matrix is NULL then get matrix
   mtx <- x$get()
+  
+  ## and calculates the inverse 
   inv <- solve(mtx, ...)
+  
+  ## sets the value of the inverse in the cache
   x$setinverse(inv)
+  
+  ## return the inverse of a matrix
   inv
 }
